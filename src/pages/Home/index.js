5ca6f7e7b0b2f5cd097bd83c26b1d9b8c115f62a
rafaellevissa <rafaellevissa@gmail.com>
2020-07-01 15:01:36 -0300

    :construction: home view

---
+++ src/pages/Home/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StatusBar} from 'react-native';
+import Styled from 'styled-components/native';
+import * as ScreenOrientation from 'expo-screen-orientation';
+
+import Videos from '../../Database/videos.json';
+
+const Container = Styled.ScrollView`
+    flex: 1;
+    backgroud-color: #333333;
+`;
+
+async function changeScreenOrientation(){
+    await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
+}
+
+export default function Home(){
+    changeScreenOrientation();
+    return(
+        <StatusBar translucent backgroundColor="transparent" barStyle="light-content">
+            <Container>
+                
+            </Container>
+        </StatusBar>
+    );
+}
+
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from "@react-navigation/stack";
+
+const AppStack = createStackNavigator();
+
+import Home from './pages/Home';
+
+export default function Route(){
+    return(
+        <NavigationContainer>
+            <AppStack.Navigator screenOptions={{ headerShown: false }}>
+                <AppStack.Screen name="Home" component={Home}/>
+            </AppStack.Navigator>
+        </NavigationContainer>
+    );
+}
